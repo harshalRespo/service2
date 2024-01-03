8b75e2894700821a52f628c191ebed885bbd1984
harshalRespo <harshal.dalvi@respo.co.in>
2024-01-03 18:15:49 +0530

    fix

--- product-app.js
+++ product-app.js
@@ -5,7 +5,7 @@ require('dotenv').config()
 
 // Define a route for the root URL
 app.get('/', (req, res) => {
-    res.send('Hello, World! from product service');
+    res.send('Hello, World! from product services');
 });
 
 // Specify the port to listen on
