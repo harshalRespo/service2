55622c1ec6c9838a4ed6beb3265a46e7752805e7
harshalRespo <harshal.dalvi@respo.co.in>
2024-01-05 15:38:38 +0530

    fix

--- product-app.js
+++ product-app.js
@@ -2,20 +2,20 @@
 const express = require('express');
 const app = express();
 require('dotenv').config()
-const rateLimit = require('express-rate-limit')
+// const rateLimit = require('express-rate-limit')
 // Define a route for the root URL
 // app.get('/', (req, res) => {
 //     res.send('Hello, World! from product services');
 // });
-const rootCtx = '/product-service/api/v2'
-const serviceRootContext = rootCtx
-const limiter = rateLimit({
-    max: 100000,
-    windowMs: 1 * 60 * 1000,
-    message: 'Too many requests from this IP, please try again in an hour!'
-})
+// const rootCtx = '/product-service/api/v2'
+// const serviceRootContext = rootCtx
+// const limiter = rateLimit({
+//     max: 100000,
+//     windowMs: 1 * 60 * 1000,
+//     message: 'Too many requests from this IP, please try again in an hour!'
+// })
 
-app.use(`${serviceRootContext}`, limiter)
+// app.use(`${serviceRootContext}`, limiter)
 app.get('/product-service/api/v2/', function (req, res) {
     res.send('Hello from product service !!')
 })
